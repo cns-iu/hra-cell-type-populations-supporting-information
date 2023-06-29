36fb5fdfe5e8bedd49a0fff73eecccb96a0bd34b
Bruce W. Herr II <bherr@indiana.edu>
2023-06-29 04:21:33 -0400

    Added logic for skipping slow queries

--- data-processor/src/gen-reports.js
+++ data-processor/src/gen-reports.js
@@ -4,6 +4,9 @@ import { basename } from 'path';
 import sh from 'shelljs';
 import { getCtPopDb } from './ctpop-db.js';
 
+// Some queries don't run well in oxigraph. This allows us to skip those for now.
+const queriesToSkip = new Set(['table-s2']);
+
 // Ensure reports output directory exists
 sh.mkdir('-p', '../data/reports');
 
@@ -13,9 +16,13 @@ const engine = await getCtPopDb();
 // Go through each query in queries, run them, and save out the csv report to ../data/reports/
 for (const queryFile of globSync('queries/*.rq')) {
   const reportName = basename(queryFile, '.rq');
-  console.log(`Creating report:`, reportName);
+  if (queriesToSkip.has(reportName)) {
+    console.log('Skipping report (for now):', reportName);
+  } else {
+    console.log('Creating report:', reportName);
 
-  const query = readFileSync(queryFile).toString();
-  const csvString = engine.selectCsv(query);
-  writeFileSync(`../data/reports/${reportName}.csv`, csvString);
+    const query = readFileSync(queryFile).toString();
+    const csvString = engine.selectCsv(query);
+    writeFileSync(`../data/reports/${reportName}.csv`, csvString);
+  }
 }
