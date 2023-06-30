d78af86c479fa9ba312446c5eedba2101af7ca8c
Bruce W. Herr II <bherr@indiana.edu>
2023-06-30 08:53:48 -0400

    Remove oxigraph dependency and just use the sparql endpoint

--- data-processor/src/gen-reports.js
+++ data-processor/src/gen-reports.js
@@ -2,32 +2,20 @@ import { readFileSync, writeFileSync } from 'fs';
 import { globSync } from 'glob';
 import { basename } from 'path';
 import sh from 'shelljs';
-import { getCtPopDb } from './ctpop-db.js';
+import { selectCsvRemote } from './sparql.js';
 
-// Some queries don't run well in oxigraph. This allows us to run with an external sparql server.
-const remoteQueries = new Set(['table-s2', 'rui-location-stats']);
+// SPARQL endpoint with ctpop data loaded
 const SPARQL_ENDPOINT = 'https://api.triplydb.com/datasets/bherr/ctpop/services/ctpop/sparql';
 
 // Ensure reports output directory exists
 sh.mkdir('-p', '../data/reports');
 
-// Initialize a SPARQL database with CTPop and HRA loaded
-const engine = await getCtPopDb();
-
 // Go through each query in queries, run them, and save out the csv report to ../data/reports/
 for (const queryFile of globSync('queries/*.rq')) {
   const reportName = basename(queryFile, '.rq');
-  if (remoteQueries.has(reportName)) {
-    console.log('Creating report via remote SPARQL Endpoint:', reportName);
+  console.log('Creating report via remote SPARQL Endpoint:', reportName);
 
-    const query = readFileSync(queryFile).toString();
-    const csvString = await engine.selectCsvRemote(query, SPARQL_ENDPOINT);
-    writeFileSync(`../data/reports/${reportName}.csv`, csvString);
-  } else {
-    console.log('Creating report:', reportName);
-
-    const query = readFileSync(queryFile).toString();
-    const csvString = engine.selectCsv(query);
-    writeFileSync(`../data/reports/${reportName}.csv`, csvString);
-  }
+  const query = readFileSync(queryFile).toString();
+  const csvString = await selectCsvRemote(query, SPARQL_ENDPOINT);
+  writeFileSync(`../data/reports/${reportName}.csv`, csvString);
 }
--- data-processor/src/sparql.js
+++ data-processor/src/sparql.js
@@ -1,135 +1,39 @@
 import { SparqlEndpointFetcher } from 'fetch-sparql-endpoint';
-import { BlankNode, Store } from 'oxigraph';
 import Papa from 'papaparse';
 
 /**
- * Extract the value from a given oxigraph node
+ * Generator that returns SPARQL query results as an array of simple objects
  *
- * @param {Node} node a node from the database
- * @returns the unwrapped value from the node
+ * @param {string} query the SPARQL query as a string
+ * @param {string} sparqlEndpoint the remote SPARQL endpoint to query
+ * @returns array of objects
  */
-function nodeValue(node) {
-  if (!node) {
-    return undefined;
-  } else if (node instanceof BlankNode) {
-    return `bn-${node.value}`;
-  } else {
-    return node.value;
-  }
+export async function selectRemoteObjects(query, sparqlEndpoint) {
+  const fetcher = new SparqlEndpointFetcher({});
+  const stream = await fetcher.fetchBindings(sparqlEndpoint, query);
+  return new Promise((resolve, reject) => {
+    const results = [];
+    stream.on('data', (bindings) => {
+      const result = Object.keys(bindings).reduce(
+        (acc, key) => ((acc[key] = bindings[key]?.value), acc),
+        {}
+      );
+      results.push(result);
+    });
+    stream.on('end', () => {
+      resolve(results);
+    });
+  });
 }
 
 /**
- * A class for running sparql queries
+ * Run a SPARQL query and return results in csv format
+ *
+ * @param {string} query the SPARQL query as a string
+ * @param {string} sparqlEndpoint the remote SPARQL endpoint to query
+ * @returns the results of the query in csv format
  */
-export class SparqlRunner {
-  constructor() {
-    this.engine = new Store();
-  }
-
-  /**
-   * @returns the underlying oxigraph engine
-   */
-  getEngine() {
-    return this.engine;
-  }
-
-  /**
-   * Run a SPARQL query and return results in the given mimetype format
-   *
-   * @param {string} query the SPARQL query as a string
-   * @param {*} mimetype the format of the returned data
-   * @returns the results of the query in the given format
-   */
-  select(query, mimetype) {
-    switch (mimetype) {
-      case 'text/tab-separated-values':
-        return this.selectTsv(query);
-      case 'text/csv':
-        return this.selectCsv(query);
-      default:
-        throw new Error(`MIME Type: ${mimetype} not supported by this method.`);
-    }
-  }
-
-  /**
-   * Generator that returns SPARQL query results as simple objects
-   *
-   * @param {string} query the SPARQL query as a string
-   */
-  *selectObjects(query) {
-    for (const datum of this.engine.query(query)) {
-      const result = {};
-      datum.forEach((node, key) => {
-        result[key] = nodeValue(node);
-      });
-      yield result;
-    }
-  }
-
-  /**
-   * Run a SPARQL query and return results in csv format
-   *
-   * @param {string} query the SPARQL query as a string
-   * @returns the results of the query in csv format
-   */
-  selectCsv(query) {
-    const data = Array.from(this.selectObjects(query));
-    return Papa.unparse(data);
-  }
-
-  /**
-   * Run a SPARQL query and return results in tsv format
-   *
-   * @param {string} query the SPARQL query as a string
-   * @returns the results of the query in tsv format
-   */
-  selectTsv(query) {
-    let fields, output;
-    for (const datum of this.engine.query(query)) {
-      if (!fields) {
-        fields = [...datum.keys()];
-        output = fields.join('\t') + '\n';
-      }
-      const row = fields.map((f) => nodeValue(datum.get(f))).filter((v) => v);
-      output += row.join('\t') + '\n';
-    }
-    return output;
-  }
-
-  /**
-   * Generator that returns SPARQL query results as an array of simple objects
-   *
-   * @param {string} query the SPARQL query as a string
-   * @param {string} sparqlEndpoint the remote SPARQL endpoint to query
-   * @returns array of objects
-   */
-  async selectRemoteObjects(query, sparqlEndpoint) {
-    const fetcher = new SparqlEndpointFetcher({});
-    const stream = await fetcher.fetchBindings(sparqlEndpoint, query);
-    return new Promise((resolve, reject) => {
-      const results = [];
-      stream.on('data', (bindings) => {
-        const result = Object.keys(bindings).reduce(
-          (acc, key) => ((acc[key] = bindings[key]?.value), acc),
-          {}
-        );
-        results.push(result);
-      });
-      stream.on('end', () => {
-        resolve(results);
-      });
-    });
-  }
-
-  /**
-   * Run a SPARQL query and return results in csv format
-   *
-   * @param {string} query the SPARQL query as a string
-   * @param {string} sparqlEndpoint the remote SPARQL endpoint to query
-   * @returns the results of the query in csv format
-   */
-  async selectCsvRemote(query, sparqlEndpoint) {
-    const data = await this.selectRemoteObjects(query, sparqlEndpoint);
-    return Papa.unparse(data);
-  }
+export async function selectCsvRemote(query, sparqlEndpoint) {
+  const data = await selectRemoteObjects(query, sparqlEndpoint);
+  return Papa.unparse(data);
 }
