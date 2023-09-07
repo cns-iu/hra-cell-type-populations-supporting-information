f9dd66861349b83ab60cfadac83b41fc9d6bbfc9
Bruce W. Herr II <bherr@indiana.edu>
2023-09-06 21:47:26 -0400

    Added Validation V1P1 plus variants

--- data-processor/src/create-blazegraph.sh
+++ data-processor/src/create-blazegraph.sh
@@ -22,6 +22,7 @@ run_jsonld() {
 }
 
 run_jsonld ../data/enriched_rui_locations.jsonld ../data/enriched_rui_locations.ttl "${CTPOP}graph"
+run_jsonld ../data/as-cell-summaries.jsonld ../data/as-cell-summaries.ttl "${CTPOP}graph"
 run_jsonld ../data/rui-location-distances.jsonld ../data/rui-location-distances.ttl "${CTPOP}distances"
 run_ndjsonld ../data/cell-summary-similarities.jsonl.gz ../data/cell-summary-similarities.ttl "${CTPOP}similarities"
 
