87a0002968ce8b5fe7bd7899632255e427b87fc5
Andreas Bueckle <abueckle@iu.edu>
2023-09-08 18:52:45 -0400

    Updated code for number check

--- data/validation+testing/number_check.py
+++ data/validation+testing/number_check.py
@@ -11,14 +11,24 @@ def main():
     enriched_rui_locations = load_json("../enriched_rui_locations.jsonld")
     rui_locations = load_json("../rui_locations.jsonld")
     corridors = load_json("../corridors.jsonld")
+    as_cell_summaries = load_json("../as-cell-summaries.jsonld")
 
     # unqiue rui locations, datasets, and cell types
     # dict for corridors and associated AS
     unique_rui_locations = set()
     unique_datasets = set()
+    unique_datasets_with_rui_locations = {
+        'bulk': set(),
+        'spatial': set()
+    }
     unique_cell_types = set()
     unique_as_in_corridors = set()
     as_counts_in_corridor = {}
+    unique_as = set()
+
+    # get unique AS
+    for ana in as_cell_summaries['@graph']:
+        unique_as.add(ana['cell_source'])
 
     # get unique datasets and cell types
     for cell_summary in dataset_cell_summaries['@graph']:
@@ -33,6 +43,15 @@ def main():
                 for collision_item in sample['rui_location']['all_collisions']:
                     for collision in collision_item['collisions']:
                         unique_as_in_corridors.add(collision['as_id'])
+
+    # get number of datasets with rui location and modality
+    for donor in enriched_rui_locations['@graph']:
+        for sample in donor['samples']:
+            for dataset in sample['datasets']:
+                if "summaries" in dataset:
+                    for sum in dataset['summaries']:
+                        unique_datasets_with_rui_locations[sum['modality']].add(dataset['@id'])
+                        # print(sum['modality'])
 
     # get numbers of AS per corridor
     for donor in enriched_rui_locations['@graph']:
@@ -52,6 +71,9 @@ def main():
           Unique enriched RUI locations: {len(count_rui_locations(enriched_rui_locations))}
           Unique tissue blocks: {len(count_tissue_blocks(enriched_rui_locations))}
           Unique AS in corridors: {len(unique_as_in_corridors)}
+          Unique datasets with RUI location (bulk): {len(unique_datasets_with_rui_locations['bulk'])}
+          Unique datasets with RUI location (spatial): {len(unique_datasets_with_rui_locations['spatial'])}
+          Unique AS summaries: {len(unique_as)}
           ''')
 
     # print result for AS per corridor
@@ -63,14 +85,15 @@ def main():
     # export result as CSV file
     with open('corridors_with_their_as.csv', 'w', newline='') as f:
         wr = csv.writer(f, delimiter=',')
-        wr.writerow(['corridor', 'number_of_unique_as','AS1','AS2', 'AS3','AS4', 'AS5','AS6', 'AS7' ])
+        wr.writerow(['corridor', 'number_of_unique_as', 'AS1',
+                    'AS2', 'AS3', 'AS4', 'AS5', 'AS6', 'AS7'])
         for key in as_counts_in_corridor:
             row = [key, len(as_counts_in_corridor[key])]
             for ana in as_counts_in_corridor[key]:
                 print(ana)
                 row.append(ana)
             wr.writerow(row)
-       
+
 
 def count_tissue_blocks(response):
     """A function to count unique tissue blocks
