14460641eecc0304a6284f9f71db768ce86d7582
Michael Ginda <michael.ginda@gmail.com>
2025-04-29 13:31:57 -0400

    Update HRA_Spatial_CellTypePer_preparation_v0.1.r

--- validations/heatmaps/HRA_Spatial_CellTypePer_preparation_v0.1.r
+++ validations/heatmaps/HRA_Spatial_CellTypePer_preparation_v0.1.r
@@ -1,12 +1,22 @@
+#### Environment Set-up ####
 library(plyr)
 library(dplyr)
 library(magrittr)
 library(stringr)
 
-metadata <- read.csv("./heatmaps/spatial/hra-pop-spatial_dataset_meta.csv",
+options(scipen = 999)
+
+#### Load Data ####
+##### Anatomical Structure Metadata ####
+# Metadata path for validation scripts
+path_validation <- paste0("./validations/")
+
+metadata <- read.csv(paste0(path_validation,"heatmaps/spatial/hra-pop-spatial_dataset_meta.csv"),
                  header = T)
+
+##### Spatial Cell Summaries ####
 # Data path
-path <- paste0("https://raw.githubusercontent.com/cns-iu/hra-ct-summaries-mx-spatial-data/main/intestine_omap_2_0001/cell_type_counts/")
+path <- paste0("https://raw.githubusercontent.com/cns-iu/hra-ct-summaries-mx-spatial-data/refs/heads/main/intestine_omap_2_0001/cell_type_counts/")
 
 # data frame for cell counts
 data <- data.frame(hubmap_id = as.character(),
@@ -24,6 +34,7 @@ for(i in 1:nrow(metadata)){
 data <- left_join(data,metadata,by="hubmap_id")
 str(data)
 
+#### Prepare Spatial Data ####
 # Calculate stats across organs + as
 # AS Cell Counts
 data_as <-
@@ -48,8 +59,9 @@ data_as_ct$percentage <- round(data_as_ct$cell_count/data_as_ct$total_cell_count
 # Update Organ Labels
 data_as_ct$organ_label <- str_replace(data_as_ct$organ_label,"LI","Large Intestine")
 data_as_ct$organ_label <- str_replace(data_as_ct$organ_label,"SI","Small Intestine")
-
 data_as_ct$tool <- "Spatial"
 
+#### Export Data Preparation Results ####
 # Save data
-write.csv(data_as_ct, file="./heatmaps/spatial/HRApop_Spatial_OASCT_CellCount_Percentage.csv", row.names = F)
+write.csv(data_as_ct, 
+          file=paste0(path_validation,"heatmaps/spatial/HRApop_Spatial_OASCT_CellCount_Percentage.csv"), row.names = F)
